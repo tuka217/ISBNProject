61df7eed038f71f5cd6746a19e863572777a0aa7
Unknown <m.rusek93@gmail.com>
2017-04-13 22:19:37 +0200

    Merge z mastera i niecałkowite wpięcie funkcjonalności w aplikację.

--- app/src/main/java/weeia/isbnapp/MainActivity.java
+++ app/src/main/java/weeia/isbnapp/MainActivity.java
@@ -1,5 +1,8 @@
 package weeia.isbnapp;
 
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,9 +11,30 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Button;
+import android.view.View;
+import android.util.SparseArray;
+import android.util.Log;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap;
+import android.app.Activity;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
+private Uri fileUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +48,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view){
                 Intent startIntent = new Intent(getApplicationContext(),BookDetailsActivity.class);
                 startActivity(startIntent);
+            }
+        });
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
             }
         });
     }
@@ -49,4 +80,55 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = getOutputMediaFileUri();
+        takePictureIntent.putExtra( MediaStore.EXTRA_OUTPUT, fileUri );
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            File imgFile = new  File(fileUri.getPath());
+            if(imgFile.exists() && imgFile.canRead()) {
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getPath());
+                //TextView txtView = (TextView) findViewById(R.id.txtContent);
+                BarcodeDetector detector =
+                        new BarcodeDetector.Builder(getApplicationContext())
+                                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                                .build();
+                if (!detector.isOperational()) {
+                    //txtView.setText("Could not set up the detector!");
+                    return;
+                }
+                Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+                SparseArray<Barcode> barcodes = detector.detect(frame);
+                if (barcodes.size() > 0) {
+                    Barcode thisCode = barcodes.valueAt(0);
+                   // txtView.setText(thisCode.rawValue);
+                } else {
+                   // txtView.setText("No barcodes recognized !");
+                }
+            }
+        }
+    }
+    private static Uri getOutputMediaFileUri(){
+        return Uri.fromFile(getOutputMediaFile());
+    }
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        boolean canRead =  Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES).canRead();
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+        return mediaFile;
+
+    }
+
 }
